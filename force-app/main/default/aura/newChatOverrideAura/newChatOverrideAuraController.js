ac242e9b7a3cbebd28ea81c0baf96c00bd877a0b
Anthony Wheeler <wheeler.anthony96@gmail.com>
2023-08-02 10:02:11 -0500

    New record closes old tab

--- force-app/main/default/aura/newChatOverrideAura/newChatOverrideAuraController.js
+++ force-app/main/default/aura/newChatOverrideAura/newChatOverrideAuraController.js
@@ -1,12 +1,24 @@
 ({
     recordCreated : function (component, event, helper) {
-        console.log('received reload event');
+        //Get Record Id
         var newId = event.getParam('result');
-        console.log(newId);
-        var navEvt = $A.get("e.force:navigateToSObject");
-        navEvt.setParams({
-          "recordId": newId
+        //Get workspace api methods
+        var workspaceAPI = component.find("workspace");
+        //Get current tab(New record page)
+        workspaceAPI.getTabInfo().then(function(response) { 
+            //Get Id of tab to close
+            var closeTabId = response.tabId;
+            //Open new record in new tab
+            workspaceAPI.openTab({
+                recordId: newId,
+                focus: true
+            }).then(function(openedTab){
+                //Close "New Record" tab page
+                workspaceAPI.closeTab({tabId: closeTabId});
+            });
+        })
+        .catch(function(error) {
+            console.log('action error: ', error.message);
         });
-        navEvt.fire();
     }
 })
